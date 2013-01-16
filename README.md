2947e4833a9493aa7ba5bcc953f4d3d303d6c4dc
Shen Yichen <2007.yichen@gmail.com>
2013-01-16 13:27:51 +0800

    Islands Brushup
    Defined everything in terms of BLOCKSIZE for scalabilty.
    Defined label to be at centroid.
    Added jQuery library to resources for direct loading.
    Removed demo (random irrelevant testing data).
    Updated Readme for instructions.

--- README.md
+++ README.md
@@ -1,4 +1,44 @@
 OLsubwaymap
 ===========
 
-Javascript app to display an interactive subway map.
+Javascript app to display an interactive subway map. Javascript reads from HTML and uses Raphael to render a map.
+
+##Introduction
+This app allows a user to render a subway map from HTML lists.
+Features includes:
+* Rendering of "stations" and 4-directional "tracks"
+* Islands to group "stations"
+* Interactive "stations" that could show additional links
+
+The map works on a grid system, with each grid taking up ??? pixels. Cooridnates are defined at the cross-sections of the grid, and __all__ settings in the HTML file should be defined in terms of grid coordinates.
+
+##Set-up
+Include the Javascript libraries in your HTML file header. They are provided in the resources folder.
+```xml
+<script type="text/javascript" src="./resources/jquery.min.js"></script>
+<script type="text/javascript" src="./resources/raphael-min.js"></script>
+```
+Include *olsubway.js* in the HTML file body
+```xml
+<script type="text/javascript" src="./olsubway.js"></script>
+```
+Create a `<div>` tag, specifying the height and width (in grid coordinates) of the map window. The map shall be placed at the position the contents of the tag resides.
+```xml
+<div ID="subway" height="50" width = "50"></div>
+```
+All further elements shall go between this tag.
+
+##Islands
+Islands can be used to group certain "stations". All islands should be defined between a `<ul ID="subway-islands"></ul>` tag.
+
+Each island is another unordered list, with each element defining an edge (with coordinates) of the island. Specify the name, background colour, label font size and colour. The name of the island will be displayed at the centroid of the polygon which defines the island. Refer to the exmaple below.
+```xml
+<ul island-name="Test" background-color="#ddd" font-size="32px" font-color="#fff">
+	<li edge="1,1"></li>
+	<li edge="18.5,1"></li>
+	<li edge="18.5,20"></li>
+	<li edge="10,25"></li>
+	<li edge="1,20"></li>
+</ul>
+```
+> As islands do not have to be defined along the grid, you may use decimals for the coordinates.
--- olsubway.js
+++ olsubway.js
@@ -2,8 +2,8 @@
 var BLOCKSIZE = 10;
 
 function Coordinate(x,y){
-	this.x = Number(x);
-	this.y = Number(y);
+	this.x = x;
+	this.y = y;
 	this.toString = function(){
 		return "("+this.x+", "+this.y+")";
 	};
@@ -14,21 +14,47 @@ function Island(name, color, fontSize, fontColor){
 	this.color = color;
 	this.fontSize = fontSize;
 	this.fontColor = fontColor;
-	this.svgUnclosed = "M";
-	this.xTotal = 0;
-	this.yTotal = 0;
-	this.edgeCount = 0;
+	this.edges = new Array();
 	this.addEdge = function(coords){
-		this.xTotal += coords.x;
-		this.yTotal += coords.y;
-		this.edgeCount++;
-		this.svgUnclosed += coords.x + ","+coords.y+" ";
+		coords = sqrToPixel(coords);
+		this.edges.push(coords);
+	}
+	this.centroid = function(){
+		try{
+			if(this.edges.length<1) throw new Error("Polygon has no points!");
+		}
+		catch(err){
+			console.error(err.name+": "+err.message);
+		}
+
+		/* Centriod forumla
+		Cx = 1/(6A) Sum(i=0->n-1){(x[i]+x[i+1])(x[i]y[i+1]-x[i+1]y[i])}
+		Cy = 1/(6A) Sum(i=0->n-1){(y[i]+y[i+1])(x[i]y[i+1]-x[i+1]y[i])}
+		A = 1/2 Sum(i=0->n-1){x[i]y[i+1]-x[i+1]y[i]}
+		*/		
+		this.edges.push(this.edges[0]);
+		var sumX=0, sumY=0, sumArea=0;
+		for(var i=0;i<this.edges.length-1;i++){
+			sumX+=(this.edges[i].x+this.edges[i+1].x)*(this.edges[i].x*this.edges[i+1].y - this.edges[i+1].x*this.edges[i].y);
+			sumY+=(this.edges[i].y+this.edges[i+1].y)*(this.edges[i].x*this.edges[i+1].y - this.edges[i+1].x*this.edges[i].y);
+			sumArea+= this.edges[i].x*this.edges[i+1].y - this.edges[i+1].x*this.edges[i].y;
+		}
+		this.edges.pop();//Remove repaeated element
+
+		return new Coordinate(1/(3*sumArea)*sumX, 1/(3*sumArea)*sumY);
+
 	}
 	this.paint = function(){
-		if(this.edgeCount>2){//Polygons have at least 3 edges
+		if(this.edges.length>2){//Polygons have at least 3 edges
 			//Draw a polygon path and label it, text in the center
-			paper.path(this.svgUnclosed+"z").attr({"fill": color, "stroke":"none"});
-			paper.text(this.xTotal/this.edgeCount,this.yTotal/this.edgeCount,this.name).attr({"font-size":fontSize, "fill": fontColor});
+			var svg="M";
+			for (i in this.edges) {
+				svg += this.edges[i].x+","+this.edges[i].y+" ";
+			}
+			paper.path(svg+"z").attr({"fill": color, "stroke":"none"});
+
+			var center = this.centroid();
+			paper.text(center.x,center.y,this.name).attr({"font-size":fontSize, "fill": fontColor});
 		}
 	}
 }
@@ -44,7 +70,7 @@ function pixelToSqr(coords){
 //Build canvas
 var main = $("#subway");
 var pos = main.position();
-var paper = Raphael(pos.left,pos.top, main.attr("width"), main.attr("height"));
+var paper = Raphael(pos.left,pos.top, main.attr("width")*BLOCKSIZE, main.attr("height")*BLOCKSIZE);
 
 //Layer 1, Islands
 $("#subway-islands").children().each(
@@ -54,7 +80,9 @@ $("#subway-islands").children().each(
 		//Add the edges
 		$(Element).children().each(
 			function(index,Element){
-				i.addEdge(new Coordinate($(Element).attr("edge-x"),$(Element).attr("edge-y")));
+				//Split the x and y and add it to the polygon
+				var edgeCoords = $(Element).attr("edge").split(",");
+				i.addEdge(new Coordinate(parseFloat(edgeCoords[0]),parseFloat(edgeCoords[1])));
 			}
 		);
 		//Paint the island
