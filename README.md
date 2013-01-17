ed6cbaf06d65740d7d73760c4f2ecdad7a0b4690
Shen Yichen <2007.yichen@gmail.com>
2013-01-18 01:22:14 +0800

    Added Tracks
    A few touchups on object proto-types too

--- README.md
+++ README.md
@@ -12,6 +12,10 @@ Features includes:
 
 The map works on a grid system, with each grid taking up ??? pixels. Cooridnates are defined at the cross-sections of the grid, and __all__ settings in the HTML file should be defined in terms of grid coordinates.
 
+###Libraries
+* Rapheal: http://raphaeljs.com/ 
+* jQuery: http://jquery.com/
+
 ##Set-up
 Include the Javascript libraries in your HTML file header. They are provided in the resources folder.
 ```xml
@@ -27,6 +31,35 @@ Create a `<div>` tag, specifying the height and width (in grid coordinates) of t
 <div ID="subway" height="50" width = "50"></div>
 ```
 All further elements shall go between this tag.
+
+##Tracks
+Tracks connect the various "stations". Currently, this app supports 4-directions for tracks (North, South, East, West), with smooth turns. All tracks should be defined between a `<ul ID="subway-tracks"></ul>` tag.
+
+Each track is another unordered list, with each element representing a segment. Specify the colour of the track, as well as the starting point.
+
+For each subsequent segment, you may:
+
+1. Define a straight link by simply specifying the coordinates of the next point.
+2. Define a curve, by specifying the coordinates of the next point, as well as the direction the track faces after the curve.
+
+Refer to the example below:
+```xml
+<ul ID="subway-tracks">
+	<ul color="#f3b" start-point="2,2">
+		<li dest="5,2"></li>
+		<li dest="6,3" turn="S"></li>
+		<li dest="6,6"></li>
+		<li dest="9,9" turn="E"></li>
+		<li dest="16,9"></li>
+		<li dest="17,10" turn="S"></li>
+		<li dest="15,12" turn="W"></li>
+		<li dest="11,16" turn="S"></li>
+		<li dest="9,18" turn="W"></li>
+		<li dest="2,18"></li>
+	</ul>
+</ul>
+```
+> Note that it is possible to create tracks that do not stick to the four directions this app is designed to handle, though it might not look the way it is intended to look like.
 
 ##Islands
 Islands can be used to group certain "stations". All islands should be defined between a `<ul ID="subway-islands"></ul>` tag.
--- olsubway.js
+++ olsubway.js
@@ -1,13 +1,15 @@
 //Magic variables here
 var BLOCKSIZE = 10;
+var TRACK_THICKNESS = 0.5;
 
 function Coordinate(x,y){
 	this.x = x;
 	this.y = y;
-	this.toString = function(){
-		return "("+this.x+", "+this.y+")";
-	};
 }
+
+Coordinate.prototype.toString = function(){
+	return this.x+","+this.y;
+};
 
 function Island(name, color, fontSize, fontColor){
 	this.name = name;
@@ -15,48 +17,93 @@ function Island(name, color, fontSize, fontColor){
 	this.fontSize = fontSize;
 	this.fontColor = fontColor;
 	this.edges = new Array();
-	this.addEdge = function(coords){
-		coords = sqrToPixel(coords);
-		this.edges.push(coords);
+}
+
+Island.prototype.addEdge = function(coords){
+	coords = sqrToPixel(coords);
+	this.edges.push(coords);
+}
+
+Island.prototype.centroid = function(){
+	try{
+		if(this.edges.length<1) throw new Error("Polygon has no points!");
 	}
-	this.centroid = function(){
-		try{
-			if(this.edges.length<1) throw new Error("Polygon has no points!");
-		}
-		catch(err){
-			console.error(err.name+": "+err.message);
-		}
-
-		/* Centriod forumla
-		Cx = 1/(6A) Sum(i=0->n-1){(x[i]+x[i+1])(x[i]y[i+1]-x[i+1]y[i])}
-		Cy = 1/(6A) Sum(i=0->n-1){(y[i]+y[i+1])(x[i]y[i+1]-x[i+1]y[i])}
-		A = 1/2 Sum(i=0->n-1){x[i]y[i+1]-x[i+1]y[i]}
-		*/		
-		this.edges.push(this.edges[0]);
-		var sumX=0, sumY=0, sumArea=0;
-		for(var i=0;i<this.edges.length-1;i++){
-			sumX+=(this.edges[i].x+this.edges[i+1].x)*(this.edges[i].x*this.edges[i+1].y - this.edges[i+1].x*this.edges[i].y);
-			sumY+=(this.edges[i].y+this.edges[i+1].y)*(this.edges[i].x*this.edges[i+1].y - this.edges[i+1].x*this.edges[i].y);
-			sumArea+= this.edges[i].x*this.edges[i+1].y - this.edges[i+1].x*this.edges[i].y;
-		}
-		this.edges.pop();//Remove repaeated element
-
-		return new Coordinate(1/(3*sumArea)*sumX, 1/(3*sumArea)*sumY);
-
+	catch(err){
+		console.error(err.name+": "+err.message);
 	}
-	this.paint = function(){
-		if(this.edges.length>2){//Polygons have at least 3 edges
-			//Draw a polygon path and label it, text in the center
-			var svg="M";
-			for (i in this.edges) {
-				svg += this.edges[i].x+","+this.edges[i].y+" ";
-			}
-			paper.path(svg+"z").attr({"fill": color, "stroke":"none"});
 
-			var center = this.centroid();
-			paper.text(center.x,center.y,this.name).attr({"font-size":fontSize, "fill": fontColor});
-		}
+	/* Centriod forumla
+	Cx = 1/(6A) Sum(i=0->n-1){(x[i]+x[i+1])(x[i]y[i+1]-x[i+1]y[i])}
+	Cy = 1/(6A) Sum(i=0->n-1){(y[i]+y[i+1])(x[i]y[i+1]-x[i+1]y[i])}
+	A = 1/2 Sum(i=0->n-1){x[i]y[i+1]-x[i+1]y[i]}
+	*/		
+	this.edges.push(this.edges[0]);
+	var sumX=0, sumY=0, sumArea=0;
+	for(var i=0;i<this.edges.length-1;i++){
+		sumX+=(this.edges[i].x+this.edges[i+1].x)*(this.edges[i].x*this.edges[i+1].y - this.edges[i+1].x*this.edges[i].y);
+		sumY+=(this.edges[i].y+this.edges[i+1].y)*(this.edges[i].x*this.edges[i+1].y - this.edges[i+1].x*this.edges[i].y);
+		sumArea+= this.edges[i].x*this.edges[i+1].y - this.edges[i+1].x*this.edges[i].y;
 	}
+	this.edges.pop();//Remove repaeated element
+
+	return new Coordinate(1/(3*sumArea)*sumX, 1/(3*sumArea)*sumY);
+}
+
+Island.prototype.paint = function(){
+	if(this.edges.length>2){//Polygons have at least 3 edges
+		//Draw a polygon path and label it, text in the center
+		var svg="M";
+		for (i in this.edges){
+			svg += this.edges[i].toString()+" ";
+		}
+		paper.path(svg+"z").attr({"fill": this.color, "stroke":"none"});
+
+		var center = this.centroid();
+		paper.text(center.x,center.y,this.name).attr({"font-size":this.fontSize, "fill": this.fontColor});
+	}
+}
+
+function Curve(dest,pt){ //pt is the point used to define the curve
+	Coordinate.call(this,dest.x, dest.y);
+	this.pt = pt;
+}
+Curve.prototype = Object.create(Coordinate.prototype);
+
+Curve.prototype.toString = function(){
+	return this.pt+" "+Coordinate.prototype.toString.call(this);
+}
+
+function Track(color, startingPt){
+	this.color = color;
+	this.segments = new Array(sqrToPixel(startingPt));
+}
+
+Track.prototype.addSegment = function(dest, dir){
+	dest = sqrToPixel(dest);
+	//Determine what type of segment is added
+	//Straight path
+	if(typeof dir == "undefined")
+		this.segments.push(dest);
+	//Horizontal to Veritcal (control = (prev.y,dest.x))
+	else if(dir.toUpperCase() == "N" || dir.toUpperCase() == "S")
+		this.segments.push(new Curve(dest, new Coordinate(dest.x,this.segments[this.segments.length-1].y)));
+	//Vertical to horizontal (control = (prev.x,dest.y))
+	else if(dir == "E" || dir == "W")
+		this.segments.push(new Curve(dest, new Coordinate(this.segments[this.segments.length-1].x,dest.y)));
+}
+
+Track.prototype.paint = function(){
+	var svg = "M"+this.segments[0]+" "; //First point is never a "curve"
+	for(var i=1;i<this.segments.length;i++){
+		//Use SVG quadratic Bézier curveto to draw curves
+		if(this.segments[i] instanceof Curve) 
+			svg+="Q"+this.segments[i]+" ";
+		//Use SVG line to draw straight lines
+		else
+			svg+="L"+this.segments[i]+" ";
+	}
+
+	paper.path(svg).attr({"stroke": this.color, "stroke-width": TRACK_THICKNESS*BLOCKSIZE});
 }
 
 function sqrToPixel(coords){
@@ -87,6 +134,24 @@ $("#subway-islands").children().each(
 		);
 		//Paint the island
 		i.paint();
+	}
+);
+
+//Layer 2, Tracks
+$("#subway-tracks").children().each(
+	function(index, Element){
+		//Build a new track
+		var startingCoord = $(Element).attr("start-point").split(",");
+		var t = new Track($(Element).attr("color"), new Coordinate(parseInt(startingCoord[0]), parseInt(startingCoord[1])));
+		//Process each segment
+		$(Element).children().each(
+			function(index,Element){
+				var destCoords = $(Element).attr("dest").split(",");
+				t.addSegment(new Coordinate(parseInt(destCoords[0]),parseInt(destCoords[1])), $(Element).attr("turn"));
+			}
+		);
+		//Paint the track
+		t.paint();
 	}
 );
 
